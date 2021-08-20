7f5c7fdfd81b3c520fd8d0adfcaa1d414e5fa856
Sasoun Torossian <sasountorossian@gmail.com>
2021-08-20 16:29:31 +0100

    Added multiple lines for main element.

--- src/Components/LineChart/LineChart.js
+++ src/Components/LineChart/LineChart.js
@@ -15,26 +15,32 @@ const LineChart = () => {
 
         console.log(timeStamps);
 
-        const margin = {top: 50, right: 30, bottom: 30, left: 30}
+        data.forEach(function(d) { d.time = new Date(d.time * 1000); });
+
+
+        const margin = {top: 50, right: 50, bottom: 30, left: 50}
         const width = parseInt(d3.select("#LineChart").style("width"))
         const height = parseInt(d3.select("#LineChart").style("height"))
 
         // Set up chart
         const svg = d3.select(d3Chart.current)
-                        .attr("width", width)
-                        .attr("height", height)
-                        .style("background-color", "red")
+                        .attr("width", 1300)
+                        .attr("height", 600)
+                        .style("background-color", "grey")
                         .append("g")
                         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
+        // x axis
         const x = d3.scaleTime()
-                    .domain(d3.extent(timeStamps, function(d){return d.date} ))
+                    .domain(d3.extent(data, function(d){return d.time} ))
                     .range([0, width])
 
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
 
-        const max = d3.max(timeStamps, function(d){return d.date})
+        // y axis
+        const max = d3.max(data, function(d){return d.high})
 
         const y = d3.scaleLinear()
                     .domain([0, max])
@@ -44,14 +50,31 @@ const LineChart = () => {
             .call(d3.axisLeft(y))
 
         svg.append("path")
-            .datum(timeStamps)
+            .datum(data)
             .attr("fill", "none")
-            .attr("stroke", "white")
-            .attr("stroke-wdth", 3)
+            .attr("stroke", "red")
+            .attr("stroke-width", 4)
             .attr("d", d3.line()
-                        .x(function(d){return x(d.date)})
-                        .y(function(d){return y(d.count)}))
+                        .x(function(d){return x(d.time)})
+                        .y(function(d){return y(d.low)}))
 
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "blue")
+            .attr("stroke-width", 4)
+            .attr("d", d3.line()
+                        .x(function(d){return x(d.time)})
+                        .y(function(d){return y(d.open)}))
+
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "green")
+            .attr("stroke-width", 4)
+            .attr("d", d3.line()
+                        .x(function(d){return x(d.time)})
+                        .y(function(d){return y(d.high)}))
     })
 
     return (
