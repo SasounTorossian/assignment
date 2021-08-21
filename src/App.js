4f220dd627a3f406d6f0dd1bd96ed05cd283a72f
Sasoun Torossian <sasountorossian@gmail.com>
2021-08-21 16:37:12 +0100

    Added Footer to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LineChart from "./Components/LineChart/LineChart"
 import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LineChart />
+      <Footer />
     </div>
   );
 }
