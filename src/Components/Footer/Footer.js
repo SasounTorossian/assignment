4062e9437c5a6ec750a33317d440db9877759858
Sasoun Torossian <sasountorossian@gmail.com>
2021-08-21 16:36:51 +0100

    Added Footer and styling

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div id="Footer"> 
+            <div>Example based on Collidr assignment</div>
+        </div>
+    )
+}
+
+export default Footer
